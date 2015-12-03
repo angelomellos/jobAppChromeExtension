d021da1a8f678b6e6b81d1dcee61a433fcfbada8
Angelo <angelo@Angelos-MacBook-Pro.local>
2015-12-02 19:37:19 -0500

    added company name and checkmarks to toast

--- content/company-page.js
+++ content/company-page.js
@@ -1,18 +1,80 @@
-$('.postings-btn').click(recordApplication);
-console.log('ran content script');
-function recordApplication() {
-  var sendObj = { "post": {
-    "title": "Testing background",
-    "excerpt": "its in...",
-    "content": "its in the background",
-    "active": true
-  } }
-  $.ajax({
-    url: "http://localhost:3000/api/post",
-    method: "POST",
-    data: sendObj,
-    success: function(data){
-      console.log('data===',data);
-    }
-  })
+// $('.postings-btn').click(recordApplication);
+// function recordApplication() {
+//   var sendObj = { "post": {
+//     "title": "Testing background",
+//     "excerpt": "its in...",
+//     "content": "its in the background",
+//     "active": true
+//   } }
+//   $.ajax({
+//     url: "http://localhost:3000/api/post",
+//     method: "POST",
+//     data: sendObj,
+//     success: function(data){
+//       console.log('data===',data);
+//     }
+//   })
+// }
+$(document).ready(function(){
+  if (document.URL.match(/job|career/)){
+    var submitButton = $("button")
+    .filter(function(idx) {
+      return this.innerHTML.match(/submit|apply/) > -1;
+    });
+  }
+  createToast();
+});
+
+function createToast(){
+  var applicationToast = document.createElement("div");
+  $(applicationToast).css({
+    "color" : "#fff",
+    "background" : "#68c368",
+    "z-index" : "2147483647px",
+    "position" : "absolute",
+    "top" : "60px",
+    "right" : "0px",
+    "padding" : "15px 30px",
+    "border-radius" : "15px",
+    "cursor" : "default",
+    "font-size" : "20px",
+  });
+  $(applicationToast).html("You've applied to ");
+  $("body").append(applicationToast);
+  var xMark = document.createElement("div");
+  var checkMark = document.createElement("div");
+  var companyInput = document.createElement("input");
+  // companyInput.type = "text";
+  $(checkMark).html("&#10004;");
+  $(checkMark).css({
+    "display" : "inline",
+    "margin-left" : "10px",
+  });
+  $(xMark).html("&#x2717;");
+  $(xMark).css({
+    "display" : "inline",
+    "margin-left" : "10px",
+  });
+  $(companyInput).css({
+    "display" : "inline",
+    "margin-left" : "10px",
+  });
+  var Url = document.URL;
+  var Url = Url.replace("https://","");
+  var Url = Url.replace("http://","");
+  var Url = Url.replace("www.","");
+  var Url = Url.replace("jobs","");
+  var Url = Url.replace("job","");
+  var Url = Url.replace("careers","");
+  if (Url.indexOf("lever") > -1) Url = Url.split("/")[1];
+  $(companyInput).attr("value", Url.split(".")[0]);
+  $(applicationToast).append(companyInput);
+  $(applicationToast).append(checkMark);
+  $(applicationToast).append(xMark);
+  $(xMark).click(function(){
+    $(applicationToast).remove();
+  });
+  $(xMark).click(function(){
+    $(applicationToast).remove();
+  });
 }
--- src/factories/glassdoor.factory.js
+++ src/factories/glassdoor.factory.js
@@ -5,11 +5,11 @@ app.factory("glassdoorFactory", function($http){
     return new Promise (function(resolve,reject){
       chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
       var activeTabUrl = tabs[0].url;
-      var replacements = ["www.","https://","http://","com/","com","io","io/"]
+      var replacements = ["www.","https://","http://","com/","com","io","io/","edu"]
   		for (var i = 0; i < replacements.length; i++) {
   		  activeTabUrl = activeTabUrl.replace(replacements[i],"");
   		}
-
+      console.log('activeTabUrl', activeTabUrl);
       chainGlassdoorQuery(null,".",0)
       .then(previous => chainGlassdoorQuery(previous,".",1))
       .then(previous => chainGlassdoorQuery(previous,"/",1))
@@ -17,6 +17,7 @@ app.factory("glassdoorFactory", function($http){
       .then(previous => chainGlassdoorQuery(previous,null,null));
 
       function chainGlassdoorQuery(previous,splitOn,segment){
+        console.log('chain: ',previous);
         if (previous) resolve(previous);
         if (!splitOn) reject("Company not found.")
         return queryGlassdoor(activeTabUrl.split(splitOn)[segment]);
@@ -29,15 +30,20 @@ app.factory("glassdoorFactory", function($http){
   function queryGlassdoor(companyName){
 		var glassdoorApiUrl = "http://api.glassdoor.com/api/api.htm?v=1&format=json&"
     +"t.p=49227&t.k=hH2UIW7yECI&action=employers&q=" + companyName;
+    console.log('url is ',glassdoorApiUrl);
 		return $http.get(glassdoorApiUrl)
     .then(response => {
+      console.log('response was ',response);
       if (!response.data.response ||
 				!response.data.response.employers.length ||
 				!response.data.response.employers[0].exactMatch){
 				return null;
 			}
-			else
-				return response.data.response.employers[0];
+			else {
+        console.log(response.data);
+        return response.data.response.employers[0];
+      }
+
     })
 	}
 
